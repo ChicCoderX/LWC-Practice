d175d65c3d2fb61ad7f489b76bc68a42a8b97b7b
Vaishnavi Kulkarni <vaishnaviaak@gmail.com>
2025-05-17 23:37:07 +0530

    input list

--- force-app/main/default/lwc/homeInputBox/homeInputBox.js
+++ force-app/main/default/lwc/homeInputBox/homeInputBox.js
@@ -1,8 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HomeInputBox extends LightningElement {
+    @track inputList = [];
+
     handleClick() {
-        // Add your click handler logic here
-        console.log('SVG icon clicked');
+        const ele = this.template.querySelector('input');
+        if (ele) {
+            if (ele.value !== '' || ele.value !== undefined) {
+                this.inputList.push(ele.value);
+                ele.value = '';
+            }
+            console.log('got the element: ' + this.inputList);
+        }
     }
 }
